45585e4f4b1bcd131be45109cdacc07abb50f9a4
Saif Chtouour <101019876+CHTSaif@users.noreply.github.com>
2024-05-31 22:19:08 +0100

    Update WebSecurityConfig.java

--- backend/src/main/java/com/esprit/jobfinder/security/WebSecurityConfig.java
+++ backend/src/main/java/com/esprit/jobfinder/security/WebSecurityConfig.java
@@ -89,6 +89,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         .authorizeHttpRequests(auth -> 
           auth.requestMatchers("/api/auth/**").permitAll()
               .requestMatchers("/api/test/**").permitAll()
+              .requestMatchers("/api/**").permitAll()
               .anyRequest().authenticated()
         );
     
