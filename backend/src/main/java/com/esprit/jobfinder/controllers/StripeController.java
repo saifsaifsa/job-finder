387432a8ebe3a7033d3ec7423082271c0a03d360
Hamza Taoujouti <hamza.taoujouti@sofrecom.com>
2024-07-05 14:49:40 +0100

    chore: nothing

--- backend/src/main/java/com/esprit/jobfinder/controllers/StripeController.java
+++ backend/src/main/java/com/esprit/jobfinder/controllers/StripeController.java
@@ -1,24 +1,16 @@
 package com.esprit.jobfinder.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.esprit.jobfinder.payload.request.CheckoutPayment;
 import com.esprit.jobfinder.services.PaymentService;
-import com.stripe.model.PaymentIntent;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.stripe.exception.StripeException;
-import com.stripe.model.checkout.Session;
-import com.stripe.param.checkout.SessionCreateParams;
 
 @RestController
 @RequestMapping(value = "/api/payment")
@@ -35,32 +27,8 @@ public class StripeController {
             throws StripeException {
 
         Session session = paymentService.createSession(paymentInfoRequest);
-//        Map<String, String> responseData = new HashMap<>();
-//        responseData.put("id", session.getId());
         String paymentStr = session.toJson();
 
         return new ResponseEntity<>(paymentStr, HttpStatus.OK);
     }
-//    @PostMapping()
-//    /**
-//     * Payment with Stripe checkout page
-//     *
-//     * @throws StripeException
-//     */
-//    public ResponseEntity<String> paymentWithCheckoutPage(@RequestBody CheckoutPayment payment) throws StripeException {
-//        try {
-//            System.out.println("Received payment request: " + payment);
-//            Map<String, Object> params = new HashMap<>();
-//            params.put("amount", payment.getAmount());
-//            params.put("currency", payment.getCurrency());
-//            params.put("quantity", payment.getQuantity());
-//            params.put("payment_method_types", new String[]{"card"});
-//            PaymentIntent paymentIntent = PaymentIntent.create(params);
-//            Map<String, String> responseData = new HashMap<>();
-//            responseData.put("clientSecret", paymentIntent.getClientSecret());
-//        return ResponseEntity.ok().body(responseData.toString());
-//        } catch (StripeException e) {
-//            return ResponseEntity.status(500).body(e.getMessage());
-//        }
-//    }
 }
