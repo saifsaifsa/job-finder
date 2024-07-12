f878f4aa290a29c142b5fcd0bfe90b88336c55bf
mohamed sallami rhouma <mohamed-sr@hotmail.com>
2024-07-12 10:01:45 +0100

    add user pie chart

--- dashboard/src/app/modules/admin/stats/stats.component.ts
+++ dashboard/src/app/modules/admin/stats/stats.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { UserService } from 'app/core/user/user.service';
+import { ApexChart, ApexNonAxisChartSeries, ApexPlotOptions,ApexOptions } from 'ng-apexcharts';
 
+export type ChartOptions = {
+  series: ApexNonAxisChartSeries;
+  chart: ApexChart;
+  labels: any;
+  plotOptions: ApexPlotOptions;
+};
 @Component({
     selector     : 'stats',
     templateUrl  : './stats.component.html',
@@ -8,6 +15,7 @@ import { UserService } from 'app/core/user/user.service';
 })
 export class StatsComponentAdmin implements OnInit 
 {
+    public userChart: Partial<ChartOptions>;
     /**
      * Constructor
      */
@@ -18,11 +26,25 @@ export class StatsComponentAdmin implements OnInit
     {
     }
     ngOnInit(): void {
+        
+        this.userChart = {
+            series: [],
+            chart: {
+              type: 'pie',
+            },
+            labels: [],
+            plotOptions: {
+              pie: {
+                expandOnClick: true
+              }
+            }
+          };
         this._userService.getStats().subscribe((data:any)=>{
             this.usersRoles = data.usersByRole
             this.totalUsers=data.totalUsers
             this.activeUsers=data.activeUsers
+            this.userChart.series = Object.values(data.usersByRole);
+            this.userChart.labels = Object.keys(data.usersByRole);
         })
     }
-
 }
