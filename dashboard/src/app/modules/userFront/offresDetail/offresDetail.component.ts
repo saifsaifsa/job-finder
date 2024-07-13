8c6e304454824077d87f3f6b0b76b64b25002191
Saif Chtouour <101019876+CHTSaif@users.noreply.github.com>
2024-07-13 10:59:00 +0100

    update

--- dashboard/src/app/modules/userFront/offresDetail/offresDetail.component.ts
+++ dashboard/src/app/modules/userFront/offresDetail/offresDetail.component.ts
@@ -1,16 +1,54 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router'; // Import ActivatedRoute
+import { OfferService } from 'app/modules/admin/offer/offerService';
 
 @Component({
     selector     : 'offresDetail',
     templateUrl  : './offresDetail.component.html',
     encapsulation: ViewEncapsulation.None
 })
-export class OffresDetailComponentAdmin
+export class OffresDetailComponentAdmin implements OnInit
 {
+    dataSource: any;
+    userId: string | null = null; 
+    offerId: string | null = null; 
+
     /**
      * Constructor
      */
-    constructor()
-    {
+    constructor(
+        private offerService: OfferService,
+        private route: ActivatedRoute ,
+        private router: Router
+    ) {}
+
+    addUserToOffer(): void {
+        this.offerService.addUserToOffer(this.offerId, this.userId).subscribe(() => {
+            // Handle success
+            this.router.navigate(['/offresUser']); 
+        }, (error) => {
+            console.error(error); 
+        });
+    }
+
+    ngOnInit(): void {
+        // Get the user from local storage
+        const user = JSON.parse(localStorage.getItem('user'));
+
+        // Get the user ID
+        this.userId= user.id;
+        // Get the 'id' from the URL
+        this.route.paramMap.subscribe(params => {
+            this.offerId = params.get('id'); // Get the 'id' parameter
+            console.log(this.offerId);
+
+            if (this.offerId) {
+                // Use the offer ID to fetch the offer details
+                this.offerService.getOfferById(this.offerId).subscribe((data) => {
+                    this.dataSource = data;
+                    console.log(this.dataSource);
+                });
+            }
+        });
     }
 }
